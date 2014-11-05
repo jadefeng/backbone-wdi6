9932a6bb8d01a911b49f7ba99b5c5c09c555fb69
jadefeng <j.feng.1991@gmail.com>
2014-11-05 12:31:27 +1100

    Zoo is open for business

--- zoo/js/zoo.js
+++ zoo/js/zoo.js
@@ -2,6 +2,7 @@
 // Lik a ruby class but done in backbone
 var Animal  = Backbone.Model.extend ( { // extend is a way of merging two models together. i.e. backbone and a new object that has our customisation
 	defaults : {
+		id: null,
 		type: 'animal',
 		ecosystem: "",
 		stripes: 0
@@ -33,10 +34,23 @@ var ZooView = Backbone.View.extend( {
 		var view = this;
 		view.$el.empty();
 		view.$el.append('<h2> Our Menagerie </h2>');
-		this.collection.each(function(animal) { // Goes through each animal in its collection and renders them 
-			var $p = $('<p/>');
-			$p.text(animal.get('type'));
-			view.$el.append($p);
+
+		var animalIndexSource = $('#animalIndex').html();
+		var animalIndexHTML = _.template(animalIndexSource);
+
+		view.collection.each(function(animal) { // Goes through each animal in its collection and renders them 
+			
+			var animalHTML = animalIndexHTML(animal.toJSON());
+			view.$el.append(animalHTML);
+			
+			// Replace the below with underscore template
+			// var $p = $('<p/>');					// IRL, this whole thing can be done with a template
+			// $p.text(animal.get('type'));
+			// var id = animal.get('id');
+			// var $a = $('<a>Click Here</a>');
+			// $a.attr('href', '#animals/' + id);
+			// $p.append($a);
+			// view.$el.append($p);
 		});	
 	},
 	greeting: function() {
@@ -44,9 +58,9 @@ var ZooView = Backbone.View.extend( {
 	}
 });
 
-var animal1 = new Animal({type: 'giraffe', ecosystem: 'savannah'});
-var animal2 = new Animal({type: 'tiger', ecosystem: 'forest'});
-var animal3 = new Animal({type: 'frog', ecosystem: 'pond'});
+var animal1 = new Animal({id: 1, type: 'giraffe', ecosystem: 'savannah'});
+var animal2 = new Animal({id: 2, type: 'tiger', ecosystem: 'forest'});
+var animal3 = new Animal({id: 3, type: 'frog', ecosystem: 'pond'});
 
 var weBoughtAZoo = new Zoo([animal1, animal2, animal3]);
 console.log(weBoughtAZoo.models);
@@ -61,10 +75,15 @@ var AppRouter = Backbone.Router.extend({
 		console.log("You reached the root page.");
 		var zooView = new ZooView({collection: weBoughtAZoo});
 		zooView.render();
+	},
+	viewAnimal: function(id) {
+		$('#main').empty();
+		console.log("You are viewing animal", id);
 	}	
 });
 
 $(document).ready(function() {
-
+	var appRouter = new AppRouter(); 	// Instantiate the router
+	Backbone.history.start();
 
 });
